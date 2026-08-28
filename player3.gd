bdedd7b2e6ff6f18b6061d24b8424f86e696ff1f
Argentavis <krithickm24@gmail.com>
2026-08-29 05:23:08 +0530

    😢 pls work

--- player3.gd
+++ player3.gd
@@ -9,15 +9,48 @@ var melee_damage = 10
 
 var can_attack = true
 
+
 func _physics_process(delta):
+	# Gravity
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
+	# Movement
 	var direction = Input.get_axis("move_left", "move_right")
-
 	velocity.x = direction * speed
 
+	# Jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_force
 
+	# Attack
+	if Input.is_action_just_pressed("attack"):
+		attack()
+
 	move_and_slide()
+
+
+func attack():
+	if not can_attack:
+		return
+
+	can_attack = false
+
+	for body in $AttackHitbox.get_overlapping_bodies():
+		if body.has_method("take_damage"):
+			body.take_damage(melee_damage)
+
+	await get_tree().create_timer(0.5).timeout
+
+	can_attack = true
+
+
+func take_damage(amount):
+	health -= amount
+
+	if health <= 0:
+		die()
+
+
+func die():
+	queue_free()
