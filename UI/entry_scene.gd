61f56e306a3fa693b5f88cd8449e6690628440a2
Rivu Ghosh <168612403+Rivu-Codez@users.noreply.github.com>
2026-08-28 13:17:15 +0530

    Scripting Main SCene
    Added animation and labels to main scene

--- UI/entry_scene.gd
+++ UI/entry_scene.gd
@@ -1,11 +1,11 @@
 extends Control
 
-@onready var label: Label = $Label
+@onready var label: Label = $Name
 
 func _ready() -> void:
 	label.modulate.a = 0.0
 	var tween = create_tween()
-	tween.tween_property(label,"modulate:a" , 1.0,1.0)
+	tween.tween_property(label,"modulate:a" , 1.0, 5.0)
 
 	
 	
