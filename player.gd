e1543aaea96439f3520ec67f14b8ef4a168b0510
Argentavis <krithickm24@gmail.com>
2026-08-29 00:49:17 +0530

    Level2_final

--- player.gd
+++ player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -500.0
 
 @export var health: int = 100
 @export var attack_cooldown: float = 0.6  # Adjust time in seconds between slashes
