38d1faeb1d65a44415861c739b02a7574e6d51a2
Rivu Ghosh <168612403+Rivu-Codez@users.noreply.github.com>
2026-08-28 13:09:50 +0530

    Made entry scene
    added image,animation to text and position

---
+++ UI/entry_scene.gd
@@ -0,0 +1,11 @@
+extends Control
+
+@onready var label: Label = $Label
+
+func _ready() -> void:
+	label.modulate.a = 0.0
+	var tween = create_tween()
+	tween.tween_property(label,"modulate:a" , 1.0,1.0)
+
+	
+	
