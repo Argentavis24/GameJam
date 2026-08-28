0d7ad09a40f0bc9cb1e0ed8b96767ec3269d5be1
Rivu Ghosh <168612403+Rivu-Codez@users.noreply.github.com>
2026-08-28 14:28:39 +0530

    Finalized Main menu design
    designed main menu and connected to entry screen

--- UI/main_menu.gd
+++ UI/main_menu.gd
@@ -1,19 +1,27 @@
 extends Control
 
 @onready var label: Label = $Name
-@onready var label1: Label = $Start
+@onready var label1: Label = $Play
+@onready var label2: Label = $Exit
+@onready var label3: Label = $Rules
 
 var animation_done := false
 
 func _ready() -> void:
 	label.modulate.a = 0.0
 	label1.modulate.a = 0.0
+	label2.modulate.a = 0.0
+	label3.modulate.a = 0.0
+	
+	
 	var tween = create_tween()
 	#tween.set_parallel(true)
 	tween.tween_interval(1)
 	tween.tween_property(label,"modulate:a" , 1.0 , 3.0)
 	tween.tween_interval(0.25)
 	tween.tween_property(label1,"modulate:a" ,1.0 , 1.0)
+	tween.tween_property(label3,"modulate:a" ,1.0 , 1.0)
+	tween.tween_property(label2,"modulate:a" ,1.0 , 1.0)
 	tween.tween_callback(func(): animation_done = true)
 	tween.tween_callback(func():
 		start_attention_pulse()
@@ -28,11 +36,12 @@ func start_attention_pulse() -> void:
 	float_tween.tween_property(label1, "position:y", start_pos.y, 0.6).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
 	
 
+'''
 func _process(delta: float) -> void:
 	if animation_done and Input.is_action_just_pressed("StartGame"):
-		get_tree().change_scene_to_file('res://UI/MainMenu.tscn')
+		get_tree().change_scene_to_file('res://main.tscn')
 		
-
+'''
 
 
 
