a67921e330ad99813bf849f6aff2eb7c07c76f8f
Rivu Ghosh <168612403+Rivu-Codez@users.noreply.github.com>
2026-08-28 13:28:19 +0530

    Added both animation

--- UI/entry_scene.gd
+++ UI/entry_scene.gd
@@ -1,11 +1,18 @@
 extends Control
 
 @onready var label: Label = $Name
+@onready var label1: Label = $Start
 
 func _ready() -> void:
 	label.modulate.a = 0.0
+	label1.modulate.a = 0.0
 	var tween = create_tween()
-	tween.tween_property(label,"modulate:a" , 1.0, 5.0)
+	#tween.set_parallel(true)
+	tween.tween_property(label,"modulate:a" , 1.0, 2.0)
+	tween.tween_interval(0.25)
+	tween.tween_property(label1,"modulate:a" , 1.0, 3.0)
+	
+
 
 	
 	
